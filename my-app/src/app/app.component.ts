5470be20cc16d46d572ad312f247aa7488138e03
Alberto Ruiz <albertoruizb14@gmail.com>
2023-11-10 18:18:40 +0100

    Test created it-1

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EI1039 Proyectojdlksfjsldfjsñkdjflsdkjfklsdjflksdf';
+  title = 'EI1039 Proyecto';
 }
