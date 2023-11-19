bcc0fa3deb3cfb215c366163ff16a5f35f3e9038
Mora152047 <147181232+Mora152047@users.noreply.github.com>
2023-11-19 18:49:02 +0100

    reducido el componente map, y corregidos pequeños detalles

--- my-app/src/app/components/map/map.component.ts
+++ my-app/src/app/components/map/map.component.ts
@@ -2,11 +2,6 @@ import { Component, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import * as L from 'leaflet';
 import { Geocoder, geocoders } from 'leaflet-control-geocoder';
-import { Subscription } from 'rxjs';
-import { Bike } from 'src/app/interfaces/bike.class';
-import { Foot } from 'src/app/interfaces/foot.class';
-import { Mobility } from 'src/app/interfaces/mobility.interface';
-import { Vehiculo } from 'src/app/interfaces/vehicle.class';
 import { MarkerService } from 'src/app/services/marker.service';
 import { MobilityService } from 'src/app/services/mobility.service';
 import { OpenRouteService } from 'src/app/services/openrouteservice.service';
@@ -20,28 +15,19 @@ import { OpenRouteService } from 'src/app/services/openrouteservice.service';
 
 export class MapComponent implements AfterViewInit{
   private map;
-  private markers: L.LatLng[];
-  private markersSubscription!: Subscription;
-  private routeSubscription!: Subscription;
   private routeLayer;
-  
-  
-  //modoTransporte!: Mobility;
-  //hayTransporteElegido: boolean;
   distanceInKMs: string | undefined;
   timeInMinutes: number | undefined;
-  
   showRouteInfo: boolean = false;
 
 
   constructor(
     private markerService: MarkerService,
     private openRouteService: OpenRouteService,
-    private route: ActivatedRoute,
+    //private route: ActivatedRoute,
     private mobilityService: MobilityService
   ) {
-    //this.hayTransporteElegido = false;
-    this.markers = []
+
   }
 
   private initMap(): void {
@@ -55,18 +41,14 @@ export class MapComponent implements AfterViewInit{
     this.initMap();
     this.markerService.makeMarkers(this.map);
     this.initGeocoderControl();
-    this.markersSubscription = this.markerService.markersSubject.subscribe(
+    this.markerService.markersSubject.subscribe(
       data => {
-        if (data != undefined && data[0]){
-          this.markers = data;
-          if(this.routeLayer){
-            this.map.removeLayer(this.routeLayer);
-          }
-        }
+        if (data != undefined && data[0] && this.routeLayer)
+          this.map.removeLayer(this.routeLayer);
       });
   }
 
-  private initGeocoderControl() {
+  private initGeocoderControl():void {
     new Geocoder({
       geocoder: new geocoders.Nominatim({
         geocodingQueryParams: {
@@ -87,33 +69,29 @@ export class MapComponent implements AfterViewInit{
     }).addTo(this.map);
   }
 
-
-
-  calculateRoute(): void {
-    if (this.markers.length == 2 && this.mobilityService.isMobilitySelected()){
-      this.routeSubscription = this.openRouteService.getDirections(this.markers[0], this.markers[1], this.mobilityService.getMobilitySelected()).subscribe({
-        next: response => {
-            if (response && response.features && response.features.length > 0) {
-              this.drawRoute(response.features[0].geometry);
-              
-              this.distanceInKMs = (response.features[0].properties.summary.distance/1000).toFixed(2);
-              this.timeInMinutes = Math.ceil(response.features[0].properties.summary.duration/60);
+   calculateRoute(): void {
+    if (this.markerService.isMaxMarkers() && this.mobilityService.isMobilitySelected()){
+      
+      this.openRouteService.setRouteData(
+        this.markerService.getStart(),
+        this.markerService.getEnd(), 
+        this.mobilityService.getMobilitySelected());
+        this.openRouteService.routeSubject.subscribe(
+          data =>{
+            if (data != undefined && data[0])
+              this.drawRoute(data[0]);
+              this.distanceInKMs = data[1] as string;
+              this.timeInMinutes = data[2] as number;
               this.showRouteInfo = true;
-            } else {
-              console.error('No se encontraron datos de ruta en la respuesta.');
-            }
-          },
-        error: err => {
-          console.error('Error al obtener la ruta:', err);
-        }
-      })
+          }
+        )
   }
 }
 
   private drawRoute(geometry: any): void{
-    if(this.routeLayer){
+    if(this.routeLayer)
       this.map.removeLayer(this.routeLayer);
-    }
+    
     this.routeLayer = L.geoJSON(geometry, {
       style: {
         color: 'purple',
