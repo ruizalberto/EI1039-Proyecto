b6824e5f75bc39830f36a917a0282aff133110ef
Alberto Ruiz <albertoruizb14@gmail.com>
2023-12-14 20:30:06 +0100

    feat(it03h08): only allow /vehicles when logged

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
 import { MapComponent } from './components/map/map.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { LoginComponent } from './components/login/login.component';
-// import { AuthguardService } from './services/authguard.service';
+import { AuthguardService } from './services/authguard.service';
 
 const routes: Routes = [
   { path: '', component: MapComponent},
-  { path: 'vehiculos', component: VehiculosComponent},
+  { path: 'vehiculos', component: VehiculosComponent, canActivate: [AuthguardService]},
   { path: 'registro', component: RegistroComponent},
   { path: 'login', component: LoginComponent},
   { path: '**', redirectTo: ''}
--- my-app/src/app/services/authguard.service.ts
+++ my-app/src/app/services/authguard.service.ts
@@ -1,23 +1,47 @@
-// import { Injectable } from '@angular/core';
-// import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
-// import { Observable, map } from 'rxjs';
-// import { UserService } from './user.service';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { map } from 'rxjs';
+import { UserService } from './user.service';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthguardService implements CanActivateFn {
+export const AuthguardService: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService: UserService = inject(UserService);
+  const router: Router = inject(Router);
+
+  return authService.getInfoUserLogged().pipe(
+    map((status) => {
+      console.log(status);
+      if (status) {
+        return true;
+      }
+
+      return router.createUrlTree(['/login']);
+    })
+  );
+};
+// export class AuthguardService {
 
 //   constructor(private userService: UserService, private router: Router) {}
 
-//   canActivateFn(): boolean {
-//     this.userService.getInfoUserLogged().subscribe(user => {
-//       if (user) {
-//         return true;
-//       } else {
-//         this.router.navigate(['/login']);
-//         return false;
-//       }
-//     }
+//   canActivate(): boolean {
+
+//     this.userService.getInfoUserLogged().pipe(
+//       map(user => {
+//         console.log("revisando...");
+//         if (user) {
+//           return true; // Usuario autenticado, permitir acceso
+//         } else {
+//           this.router.navigate(['/login']); // Usuario no autenticado, redirigir a la página de inicio de sesión
+//           return false;
+//         }
+//       })
+//     );
+//     return false;
 //   }
 // }
+
+// export const authGuardGuard: CanActivateFn = (route, state) => {
+//   return inject(AuthguardService).canActivate();
+// };
